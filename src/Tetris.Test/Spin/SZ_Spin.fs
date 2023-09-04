0d10bad50850f68c6c499fb3ed4cb8dd70ef9657
RyushiAok <55625375+RyushiAok@users.noreply.github.com>
2023-09-04 21:49:27 +0900

    test: z-spin

--- src/Tetris.Test/Spin/SZ_Spin.fs
+++ src/Tetris.Test/Spin/SZ_Spin.fs
@@ -39,6 +39,36 @@ let ``S-Spin Double 1`` () =
     actual |> should equal expectedBoard
 
 [<Fact>]
+let ``Z-Spin Double 1`` () =
+    let board =
+        createBoardMatrix [
+            //0  1  2  3  4  5  6  7  8  9
+            [ 0; 0; 0; 0; 0; 0; 0; 0; 0; 0 ] //2
+            [ 1; 1; 1; 1; 0; 0; 1; 1; 1; 1 ]
+            [ 1; 1; 1; 1; 1; 0; 0; 1; 1; 1 ]
+        ]
+
+    let t_mino =
+        let x, y = 5, 2
+
+        Tetrimino.create (x + 3, 21 - y) Shape.Z
+        |> Tetrimino.rotateLeft board
+        |> Tetrimino.rotateLeft board
+
+    let actual =
+        TetrisBoard.setTetrimino t_mino board
+        |> fun res -> res.newBoard
+
+
+    let expectedBoard =
+        createBoardMatrix [
+            //0  1  2  3  4  5  6  7  8  9
+            [ 0; 0; 0; 0; 0; 0; 0; 0; 0; 0 ] //2
+        ]
+
+    actual |> should equal expectedBoard
+
+[<Fact>]
 let ``S-Spin Double 2`` () =
     let board =
         createBoardMatrix [
@@ -73,6 +103,39 @@ let ``S-Spin Double 2`` () =
 
 
 [<Fact>]
+let ``Z-Spin Double 2`` () =
+    let board =
+        createBoardMatrix [
+            //0  1  2  3  4  5  6  7  8  9
+            [ 0; 0; 0; 0; 0; 0; 0; 0; 0; 0 ] //3
+            [ 1; 1; 1; 1; 0; 0; 1; 1; 1; 1 ]
+            [ 1; 1; 1; 1; 0; 0; 1; 1; 1; 1 ]
+            [ 1; 1; 1; 1; 1; 0; 0; 1; 1; 1 ]
+        ]
+
+    let t_mino =
+        let x, y = 4, 3
+
+        Tetrimino.create (x + 3, 21 - y) Shape.Z
+        |> Tetrimino.rotateRight board
+        |> Tetrimino.moveDown board
+        |> Tetrimino.rotateRight board
+
+    let actual =
+        TetrisBoard.setTetrimino t_mino board
+        |> fun res -> res.newBoard
+
+
+    let expectedBoard =
+        createBoardMatrix [
+            //0  1  2  3  4  5  6  7  8  9
+            [ 0; 0; 0; 0; 0; 0; 0; 0; 0; 0 ] //3
+            [ 1; 1; 1; 1; 0; 0; 1; 1; 1; 1 ]
+        ]
+
+    actual |> should equal expectedBoard
+
+[<Fact>]
 let ``S-Spin Triple`` () =
     let board =
         createBoardMatrix [
@@ -97,8 +160,38 @@ let ``S-Spin Triple`` () =
     let expectedBoard =
         createBoardMatrix [
             //0  1  2  3  4  5  6  7  8  9
-            [ 0; 0; 0; 0; 0; 0; 0; 0; 0; 0 ] //3
-            [ 0; 0; 0; 0; 0; 0; 1; 0; 0; 0 ]
+            [ 0; 0; 0; 0; 0; 0; 1; 0; 0; 0 ] //3
+        ]
+
+    actual |> should equal expectedBoard
+
+
+[<Fact>]
+let ``Z-Spin Triple`` () =
+    let board =
+        createBoardMatrix [
+            //0  1  2  3  4  5  6  7  8  9
+            [ 0; 0; 0; 1; 0; 0; 0; 0; 0; 0 ] // 3
+            [ 1; 1; 1; 1; 1; 0; 1; 1; 1; 1 ]
+            [ 1; 1; 1; 1; 0; 0; 1; 1; 1; 1 ]
+            [ 1; 1; 1; 1; 0; 1; 1; 1; 1; 1 ]
+        ]
+
+    let t_mino =
+        let x, y = 4, 3
+
+        Tetrimino.create (x + 3, 21 - y) Shape.Z
+        |> Tetrimino.rotateRight board
+
+    let actual =
+        TetrisBoard.setTetrimino t_mino board
+        |> fun res -> res.newBoard
+
+
+    let expectedBoard =
+        createBoardMatrix [
+            //0  1  2  3  4  5  6  7  8  9
+            [ 0; 0; 0; 1; 0; 0; 0; 0; 0; 0 ] //3
         ]
 
     actual |> should equal expectedBoard
