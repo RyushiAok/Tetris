aadbaddb7b6c575bbf3255412991f29659611513
RyushiAok <55625375+RyushiAok@users.noreply.github.com>
2023-09-04 23:35:20 +0900

    test: i-spin

---
+++ src/Tetris.Test/I_Spin.fs
@@ -0,0 +1,162 @@
+module ``I-Spin Test``
+// https://tetrisch.github.io/main/spins.html
+
+open Xunit
+open FsUnit.Xunit
+open Tetris.Core
+
+open Spin.Util
+
+
+[<Fact>]
+let ``I-Spin 1`` () =
+    let board =
+        createBoardMatrix [
+            //0  1  2  3  4  5  6  7  8  9
+            [ 1; 1; 1; 1; 0; 0; 0; 0; 0; 0 ]
+            [ 1; 1; 1; 0; 0; 0; 0; 0; 0; 0 ] // 3
+            [ 1; 1; 1; 0; 1; 1; 1; 1; 1; 1 ]
+            [ 1; 1; 1; 0; 1; 1; 1; 1; 1; 1 ]
+            [ 1; 1; 1; 0; 1; 1; 1; 1; 1; 1 ]
+        ]
+
+    let t_mino =
+        let x, y = 6, 3
+
+        Tetrimino.create (x + 3, 21 - y) Shape.I
+        |> Tetrimino.moveLeft board
+        |> Tetrimino.rotateRight board
+
+    let actual =
+        TetrisBoard.setTetrimino t_mino board
+        |> fun res -> res.newBoard
+
+
+    let expectedBoard =
+        let i = cell2int[Mino Shape.I]
+
+        createBoardMatrix [
+            //0  1  2  3  4  5  6  7  8  9
+            [ 1; 1; 1; 1; 0; 0; 0; 0; 0; 0 ]
+            [ 1; 1; 1; i; 0; 0; 0; 0; 0; 0 ] // 3
+        ]
+
+    actual |> should equal expectedBoard
+
+
+[<Fact>]
+let ``I-Spin 2`` () =
+    let board =
+        createBoardMatrix [
+            //0  1  2  3  4  5  6  7  8  9
+            [ 0; 0; 0; 0; 0; 0; 1; 1; 1; 1 ]
+            [ 0; 0; 0; 0; 0; 0; 0; 1; 1; 1 ] // 3
+            [ 1; 1; 1; 1; 1; 1; 0; 1; 1; 1 ]
+            [ 1; 1; 1; 1; 1; 1; 0; 1; 1; 1 ]
+            [ 1; 1; 1; 1; 1; 1; 0; 1; 1; 1 ]
+        ]
+
+    let t_mino =
+        let x, y = 4, 3
+
+        Tetrimino.create (x + 3, 21 - y) Shape.I
+        |> Tetrimino.rotateRight board
+        |> Tetrimino.rotateRight board
+        |> Tetrimino.rotateLeft board
+
+    let actual =
+        TetrisBoard.setTetrimino t_mino board
+        |> fun res -> res.newBoard
+
+
+    let expectedBoard =
+        let i = cell2int[Mino Shape.I]
+
+        createBoardMatrix [
+            //0  1  2  3  4  5  6  7  8  9
+            [ 0; 0; 0; 0; 0; 0; 1; 1; 1; 1 ]
+            [ 0; 0; 0; 0; 0; 0; i; 1; 1; 1 ] // 3
+        ]
+
+    actual |> should equal expectedBoard
+
+
+
+[<Fact>]
+let ``I-Spin 3`` () =
+    let board =
+        createBoardMatrix [
+            //0  1  2  3  4  5  6  7  8  9
+            [ 0; 0; 0; 0; 0; 0; 0; 0; 0; 0 ] // 4
+            [ 1; 1; 1; 0; 0; 0; 0; 0; 0; 0 ]
+            [ 1; 1; 1; 0; 0; 0; 0; 1; 1; 1 ] // 2
+            [ 1; 1; 1; 0; 1; 1; 1; 1; 1; 1 ]
+            [ 1; 1; 1; 0; 0; 0; 0; 1; 1; 1 ]
+        ]
+
+    let t_mino =
+        let x, y = 3, 4
+
+        Tetrimino.create (x + 3, 21 - y) Shape.I
+        |> Tetrimino.rotateRight board
+        |> Tetrimino.moveDown board
+        |> Tetrimino.moveDown board
+        |> Tetrimino.moveDown board
+        |> Tetrimino.rotateRight board
+
+    let actual =
+        TetrisBoard.setTetrimino t_mino board
+        |> fun res -> res.newBoard
+
+
+    let expectedBoard =
+
+        createBoardMatrix [
+            //0  1  2  3  4  5  6  7  8  9
+            [ 1; 1; 1; 0; 0; 0; 0; 0; 0; 0 ] // 3
+            [ 1; 1; 1; 0; 0; 0; 0; 1; 1; 1 ]
+            [ 1; 1; 1; 0; 1; 1; 1; 1; 1; 1 ]
+        ]
+
+    actual |> should equal expectedBoard
+
+
+
+[<Fact>]
+let ``I-Spin 4`` () =
+    let board =
+        createBoardMatrix [
+            //0  1  2  3  4  5  6  7  8  9
+            [ 0; 0; 0; 0; 0; 0; 0; 0; 0; 0 ] // 4
+            [ 0; 0; 0; 0; 0; 0; 0; 1; 1; 1 ]
+            [ 1; 1; 1; 0; 0; 0; 0; 1; 1; 1 ] // 2
+            [ 1; 1; 1; 1; 1; 1; 0; 1; 1; 1 ]
+            [ 1; 1; 1; 0; 0; 0; 0; 1; 1; 1 ]
+        ]
+
+    let t_mino =
+        let x, y = 6, 4
+
+        Tetrimino.create (x + 3, 21 - y) Shape.I
+        |> Tetrimino.rotateLeft board
+        |> Tetrimino.moveDown board
+        |> Tetrimino.moveDown board
+        |> Tetrimino.moveDown board
+        |> Tetrimino.rotateLeft board
+
+    let actual =
+        TetrisBoard.setTetrimino t_mino board
+        |> fun res -> res.newBoard
+
+
+    let expectedBoard =
+
+        createBoardMatrix [
+            //0  1  2  3  4  5  6  7  8  9
+            [ 0; 0; 0; 0; 0; 0; 0; 0; 0; 0 ] // 4
+            [ 0; 0; 0; 0; 0; 0; 0; 1; 1; 1 ]
+            [ 1; 1; 1; 0; 0; 0; 0; 1; 1; 1 ] // 2
+            [ 1; 1; 1; 1; 1; 1; 0; 1; 1; 1 ]
+        ]
+
+    actual |> should equal expectedBoard
